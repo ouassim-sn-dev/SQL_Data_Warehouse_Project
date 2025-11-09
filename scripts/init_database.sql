07be5e0bd360377136861202259e96d0ce7eed1c
Ouassim_sebbani <wassimsebbani18@gmail.com>
2025-11-09 15:58:09 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,46 @@
+/*
+=============================================================
+              Création de la Base de Données et des Schémas
+=============================================================
+Utilité de ce script :
+    Ce script sert à créer une base de données appelée "DataWarehouse"
+    et vérifie si elle existe déjà.
+    Si la base existe, elle est supprimée puis recréée.
+    Le script crée également trois schémas dans la base :
+    'bronze', 'silver' et 'gold'.
+
+ATTENTION :
+    L’exécution de ce script supprimera entièrement la base de données
+    "DataWarehouse" si elle existe.
+    Toutes les données qu’elle contient seront définitivement perdues.
+    Veillez à faire une sauvegarde avant d’exécuter ce script.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate la base des donnees 'DataWarehouse'
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- La creation de la base des donnees 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
 
